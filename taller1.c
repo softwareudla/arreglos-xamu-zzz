ec72eba6049282a0e6d7a157d35493eb1840820d
samuel cobo <samuel.cobo@udla.edu.ec>
2024-10-02 01:44:51 -0500

    inicio

--- taller1.c
+++ taller1.c
@@ -2,18 +2,13 @@
 
 int main (int argc, char *argv[]) {
 
-    char nombres[4][20];
+    char nombre[4][20];
 
     for (int i = 0; i < 4; i++)
     {
         printf("ingrese el nombre del estudiante%d", i);
         //scanf("%s", &nombres[i]);
-        fgets(nombres[i],20,stdin);
-    }
-
-    for (int i = 0; i < 4; i++)
-    {
-        printf("%s", nombres[i]);
+        fgets(nombre[i],20,stdin);
     }
     
     char asig[3][20];
@@ -25,10 +20,22 @@ int main (int argc, char *argv[]) {
         fgets(asig[i],20,stdin);
     }
 
-    for (int i = 0; i < 3; i++)
+   for (int i = 0; i < 4; i++)
     {
-        printf("%s", asig[i]);
+        printf("notas del estudiante: %s en %s en %s en %s", nombre[i], asig[0],asig[1],asig[2]);
     }
+    
+     //imprime
+    for (int i = 0; i < 4; i++) 
+    {
+       
+        printf("notas del estudiante: %s en %s=  en %d en %s", nombre[i], asig[1], nota[i], asig[2],asig[3]);
+    }
+    
+float maxA[6][3], maxE[6][3];
+    float minA[6][3], minE[6][3];
+    float promA[6][3];
+    float promE[6][3];
 
     
     return 0;
